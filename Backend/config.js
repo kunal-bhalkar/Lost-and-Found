4a52605927e85bdbfc34d22fb6699961a4dd2799
Kunal Bhalkar <bhalkarkunaluday@gmail.com>
2025-04-19 16:48:18 +0530

    Add files via upload

---
+++ Backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = process.env.PORT || 8000;
+
+export const mongoURL ="mongodb+srv://Chauraksha:Sakshi@cluster0.yup6d.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
---
+++ Frontend/src/components/About.jsx
@@ -0,0 +1,21 @@
+function About(){
+    return (
+      <section id="about">
+        <h1 className="lfh1">About Us</h1>
+        <p className="about-desc">
+          Welcome to our online community dedicated to helping you find lost
+          items and reconnect with cherished possessions. At <span >Lost and Found</span>, we
+          understand the heartache and frustration that losing something
+          valuable can bring. We firmly
+          believe that by coming together, we can increase the chances of
+          reuniting lost items with their rightful owners.
+        </p>
+        <footer>
+            <p>Copyright &copy; 2024</p>
+            <p>Designed and Built by <a href=""></a>Team Avinya</p>
+        </footer>
+      </section>
+      
+    );
+}
+export default About;
